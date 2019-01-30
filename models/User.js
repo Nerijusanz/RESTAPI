0cd38d9412bce3d5bc92544508756d818c2c96ec
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-30 11:30:47 +0200

    ./models/User.js UserSchema updated

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "name field is required"]
@@ -14,4 +14,4 @@ const schema = new mongoose.Schema({
   }
 });
 
-export default mongoose.model("User", schema);
+export default mongoose.model("User", UserSchema);
